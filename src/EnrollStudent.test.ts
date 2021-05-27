55a976c08aa6b51c59b1c9bf83a83e73ec92d89b
Victtor <victtorfreitas95@gmail.com>
2021-05-26 21:16:32 -0300

    Teste se o cpf estudante é valido

--- src/EnrollStudent.test.ts
+++ src/EnrollStudent.test.ts
@@ -13,3 +13,16 @@ test("Não deve matricular sem um nome de estudante válido", () => {
             .toThrow(new Error("Nome do estudante invalido!"));
     }
 );
+
+test("Não deve matricular sem um cpf de estudante válido", () => {
+    const enrollStudent = new EnrollStudent();
+    const enrollmentRequest = {
+        student: {
+            name: "Ana Clara",
+            cpf: "027.297.121-00"
+        }
+    }
+
+    expect(() => enrollStudent.execute(enrollmentRequest))
+        .toThrow(new Error("Cpf do estudante invalido!"));
+})
