a587edea08aa1101f6a0f0acbae7e8776bab6645
Victtor <victtorfreitas95@gmail.com>
2021-05-26 21:57:40 -0300

    Refatora testes para uso de entidades

--- src/EnrollStudent.test.ts
+++ src/EnrollStudent.test.ts
@@ -4,12 +4,8 @@ import Student from "./Student";
 
 test("Não deve matricular sem um nome de estudante válido", () => {
         const enrollStudent = new EnrollStudent();
-        const enrollmentRequest = {
-            student: {
-                name: "Ana",
-                cpf: "123.456.789-00"
-            }
-        }
+        let student = new Student("Ana", "027.297.121-94");
+        const enrollmentRequest = new EnrollmentRequest(student);
 
         expect(() => enrollStudent.execute(enrollmentRequest))
             .toThrow(new Error("Nome do estudante invalido!"));
@@ -18,12 +14,8 @@ test("Não deve matricular sem um nome de estudante válido", () => {
 
 test("Não deve matricular sem um cpf de estudante válido", () => {
     const enrollStudent = new EnrollStudent();
-    const enrollmentRequest = {
-        student: {
-            name: "Ana Clara",
-            cpf: "027.297.121-00"
-        }
-    }
+    let student = new Student("Ana Clara", "027.297.121-00");
+    const enrollmentRequest = new EnrollmentRequest(student);
 
     expect(() => enrollStudent.execute(enrollmentRequest))
         .toThrow(new Error("Cpf do estudante invalido!"));
