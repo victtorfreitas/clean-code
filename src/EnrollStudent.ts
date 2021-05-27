8cef12b9af77e36f85bd39174cd7005fa54dbfb3
Victtor <victtorfreitas95@gmail.com>
2021-05-26 21:18:20 -0300

    Adiciona validacao cpf a EnrollStudent

--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -1,8 +1,26 @@
+import CpfValidation from "./CpfValidation";
+
 export default class EnrollStudent {
 
     execute(enrollmentRequest: any) {
-        if (!/^([A-Za-z]+ )+([A-Za-z])+$/.test(enrollmentRequest.student.name)) {
+        this.validateRequest(enrollmentRequest);
+
+    }
+
+    validateRequest(enrollmentRequest: any) {
+        this.validateName(enrollmentRequest.student.name);
+        this.validateCpf(enrollmentRequest.student.cpf);
+    }
+
+    private validateName(name: string) {
+        if (!/^([A-Za-z]+ )+([A-Za-z])+$/.test(name)) {
             throw new Error("Nome do estudante invalido!");
+        }
+    }
+
+    private validateCpf(cpf: string) {
+        if(!CpfValidation.execute(cpf)){
+            throw new Error("Cpf do estudante invalido!");
         }
     }
 }
