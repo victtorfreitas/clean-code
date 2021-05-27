2a7cae127449f2970cc3177e3349be13ac2eb07a
Victtor <victtorfreitas95@gmail.com>
2021-05-26 21:14:08 -0300

    Atualiza teste se nome estudante esta valido

--- src/EnrollStudent.test.ts
+++ src/EnrollStudent.test.ts
@@ -1,12 +1,15 @@
 import EnrollStudent from "./EnrollStudent";
 
-test("Não deve matricular sem um nome de estudante válido", function () {
+test("Não deve matricular sem um nome de estudante válido", () => {
         const enrollStudent = new EnrollStudent();
         const enrollmentRequest = {
             student: {
-                name: "Ana"
+                name: "Ana",
+                cpf: "123.456.789-00"
             }
         }
-        expect(() => enrollStudent.execute(enrollmentRequest)).toThrow(new Error("Nome do estudante invalido!"))
+
+        expect(() => enrollStudent.execute(enrollmentRequest))
+            .toThrow(new Error("Nome do estudante invalido!"));
     }
 );
