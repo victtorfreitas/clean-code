88b1795e0d89977d247ced5ab46081c566151c51
Victtor <victtorfreitas95@gmail.com>
2021-05-30 18:14:11 -0300

    Adiciona data aniversário e numero matricula

--- src/Student.ts
+++ src/Student.ts
@@ -1,9 +1,12 @@
 export default class Student {
     name: string;
     cpf: string;
+    birthDate: Date;
+    enrollNumber: string = "";
 
-    constructor(name: string, cpf: string) {
+    constructor(name: string, cpf: string, birthDate: Date) {
         this.name = name;
         this.cpf = cpf;
+        this.birthDate = birthDate;
     }
 }
