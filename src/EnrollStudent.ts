a99cface7b0226b1f3aebe4d01baa5298edac3bc
Victtor <victtorfreitas95@gmail.com>
2021-05-30 18:52:24 -0300

    Refatora EnrollStudent

--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -1,45 +1,22 @@
-import CpfValidation from "./CpfValidation";
-import StudentRepository from "./StudentRepository";
-import Student from "./Student";
+import StudentRepository from "./Student.repository";
 import EnrollmentRequest from "./EnrollmentRequest";
+import {StudentValidation} from "./Student.validation";
 
 export default class EnrollStudent {
 
     private studentRepository: StudentRepository;
+    private studentValidation: StudentValidation;
 
     constructor() {
         this.studentRepository = new StudentRepository();
+        this.studentValidation = new StudentValidation(this.studentRepository);
     }
 
     execute(enrollmentRequest: EnrollmentRequest): string {
-        this.validateRequest(enrollmentRequest);
+        this.studentValidation.execute(enrollmentRequest.student);
         this.enroll(enrollmentRequest);
 
         return enrollmentRequest.student.enrollNumber;
-    }
-
-    validateRequest(enrollmentRequest: any) {
-        this.validateName(enrollmentRequest.student.name);
-        this.validateCpf(enrollmentRequest.student.cpf);
-        this.validateUnique(enrollmentRequest.student);
-    }
-
-    private validateName(name: string) {
-        if (!/^([A-Za-z]+ )+([A-Za-z])+$/.test(name)) {
-            throw new Error("Nome do estudante invalido!");
-        }
-    }
-
-    private validateCpf(cpf: string) {
-        if (!CpfValidation.execute(cpf)) {
-            throw new Error("Cpf do estudante invalido!");
-        }
-    }
-
-    private validateUnique(student: Student) {
-        if (this.studentRepository.findByCpf(student.cpf)) {
-            throw new Error("Não deve matricular um aluno duplicado");
-        }
     }
 
     private enroll(enrollmentRequest: EnrollmentRequest) {
