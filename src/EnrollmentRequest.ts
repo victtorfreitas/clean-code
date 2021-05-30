6c510c201210d6605be96205da4f21962dd1e84d
Victtor <victtorfreitas95@gmail.com>
2021-05-30 18:19:37 -0300

    Adiciona level, modulo e classe EnrollmentRequest

--- src/EnrollmentRequest.ts
+++ src/EnrollmentRequest.ts
@@ -2,8 +2,19 @@ import Student from "./Student";
 
 export default class EnrollmentRequest {
     student: Student;
+    level: string;
+    module: string;
+    clasz: string;
 
-    constructor(student: Student) {
+    constructor(student: Student, level: string, module: string, clasz: string) {
         this.student = student;
+        this.level = level;
+        this.module = module;
+        this.clasz = clasz;
+    }
+
+    public generetedEnrollNumber = (sequence: string): string => {
+        const currentYear = new Date().getFullYear();
+        return `${currentYear}${this.level}${this.module}${this.clasz}${sequence}`
     }
 }
