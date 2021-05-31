1c43bd6aae5aec551605160c956537c8b378a30c
Danilo <danilofreitas.dev@gmail.com>
2021-05-30 22:14:42 -0300

    Valida se a idade minima é respeitada

--- src/service/EnrollStudent.service.ts
+++ src/service/EnrollStudent.service.ts
@@ -1,28 +1,30 @@
-import StudentRepository from "./Student.repository";
-import EnrollmentRequest from "./EnrollmentRequest";
-import {StudentValidation} from "./Student.validation";
+import StudentRepository from "../repository/Student.repository";
+import EnrollmentRequest from "../model/EnrollmentRequest";
+import {StudentValidation} from "../validation/Student.validation";
+import EnrollmentRequestValidation from "../validation/EnrollmentRequest.validation";
 
-export default class EnrollStudent {
+export default class EnrollStudentService {
 
     private studentRepository: StudentRepository;
     private studentValidation: StudentValidation;
+    private enrollmentRequestValidation: EnrollmentRequestValidation;
 
     constructor() {
         this.studentRepository = new StudentRepository();
         this.studentValidation = new StudentValidation(this.studentRepository);
+        this.enrollmentRequestValidation = new EnrollmentRequestValidation();
     }
 
     execute(enrollmentRequest: EnrollmentRequest): string {
         this.studentValidation.execute(enrollmentRequest.student);
+        this.enrollmentRequestValidation.execute(enrollmentRequest);
         this.enroll(enrollmentRequest);
-
         return enrollmentRequest.student.enrollNumber;
     }
 
     private enroll(enrollmentRequest: EnrollmentRequest) {
         const student = enrollmentRequest.student;
         student.enrollNumber = enrollmentRequest.generetedEnrollNumber(this.studentRepository.getNextSequence());
-
         this.studentRepository.persist(student);
     }
 }
